bee5812bd879b5070ed1378fe86313b49ff6037c
Prashant Kumar <prashantku77@gmail.com>
2024-09-06 10:35:31 +0530

    new changes morning

---
+++ src/JavaPrograms/Swapintwithoutusingthirdvariable.java
@@ -0,0 +1,17 @@
+package JavaPrograms;
+
+public class Swapintwithoutusingthirdvariable {
+
+	public static void main(String[] args) {
+		int a=59;
+		int b=99;
+		
+       a=a+b;
+       b=a-b;
+       a=a-b;
+       System.out.println("Value of a is::"+a);
+       System.out.println("Value of b is::"+b);
+		
+	}
+
+}
