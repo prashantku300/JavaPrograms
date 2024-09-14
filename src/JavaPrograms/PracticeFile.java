125d220b93b403f560e80cf886eb8fe773055800
Prashant Kumar <prashantku77@gmail.com>
2024-09-14 23:56:03 +0530

    New Practice changes

---
+++ src/JavaPrograms/PracticeFile.java
@@ -0,0 +1,25 @@
+package JavaPrograms;
+
+public class PracticeFile {
+
+	public static void main(String[] args) {
+
+		for(int i=0; i<=4; i++) {
+			for(int j=0; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for(int i=0; i<=4; i++) {
+			for(int j=4; j>i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		
+		}
+		
+		
+	}
+
+}
